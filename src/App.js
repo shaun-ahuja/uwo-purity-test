fc11e5a2b2243e1d3e9474df4813fa13ff72168f
ShaunAhuja <sahuja47@uwo.ca>
2025-03-10 21:01:57 -0400

    adding vercel analytics code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 
